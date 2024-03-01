8b5322cc469d046068d7f3686ae67dbf1404e8f2
jamalabd <jamalabdul47@gmail.com>
2024-03-01 18:47:29 -0500

    feat: navbar active

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import Links from "@/components/navbar/links/Links"
+import styles from './navbar.module.css'
 
 const  Navbar = ()=> {
   return (
-    <>
-      <div> Logo </div>
+    <div className={styles.container}>
+      <div className={styles.logo} > Logo </div>
      <Links /> 
-    </>
+    </div>
 
 
   )
--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,14 +1,27 @@
+"use client"
+
 import React from 'react'
-import Link from 'next/link';
+import NavLink from '@/components/navbar/links/navLink/NavLink'
+import styles from './links.module.css'
 
 const links = [
-    {title: 'Home', href: '/'},{title: 'About', href: '/about'},{title: 'Contact', href: '/contact'}, { title: 'Blog', href: '/blog'}, { title: 'Login', href: '/login'},
+    {title: 'Home', href: '/'},{title: 'About', href: '/about'},{title: 'Contact', href: '/contact'}, { title: 'Blog', href: '/blog'},
 ];
 
+const session = true;
+const isAdmin = true;
+
 const Links = () => {
+  const [open, setOpen] = React.useState(false);
+
   return (
-    <div>
-        {links.map(link => ( <Link key={link.title} href={link.href}>{link.title}</Link> ))}
+    <div className={styles.links}>
+       {links.map(item => <NavLink item={item} key={item.title}></NavLink>)}{session ? (
+        <>
+        {isAdmin && <NavLink item={{title: 'Admin', href: '/admin'}}></NavLink>}
+        <button className={styles.logout}>Logout</button>
+        </>
+       ) : ( <NavLink item={{title: 'Login', href: '/login'}}></NavLink>)}
     </div>
   )
 }
